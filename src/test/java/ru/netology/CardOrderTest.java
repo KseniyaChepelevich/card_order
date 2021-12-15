cdd175b36465edde9eb1ddd2b329116a4fb59fbc
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2021-12-15 23:45:20 +0300

    added Test shouldSendTheCompletedForm

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-class CardOrderTest {
+import java.util.List;
 
-    WebDriver driver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardOrderTest {
+
+    private WebDriver driver;
 
     @BeforeAll
     static void setupClass() {
@@ -31,15 +37,41 @@ class CardOrderTest {
 
     @AfterEach
     void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
+
+        driver.quit();
+        driver = null;
+
     }
 
     @Test
-    public void shouldSendForm() {
+    public void shouldSendForm() throws InterruptedException {
 
         driver.get("http://localhost:9999");
         System.out.println("");
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldSendTheCompletedForm() {
+
+        driver.get("http://localhost:9999/");
+        //List<WebElement> elements = driver.findElements(By.className("input__control"));
+        WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        //elements.get(0).sendKeys("Васильева Анна");
+        //elements.get(1).sendKeys("+79258886611");
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Васильева Анна");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79258886611");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.className("button button_view_extra button_size_m button_theme_alfa-on-white")).click();
+        //driver.findElement(By.className("checkbox__control")).click();
+        //driver.findElement(By.className("button button_view_extra button_size_m button_theme_alfa-on-white")).click();
+        String text = driver.findElements(By.className("paragraph paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+        //
+
+
+
+        //form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
     }
 }
